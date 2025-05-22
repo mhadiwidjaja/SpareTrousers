e89ca784afacd7613f91245a5681ef3c5a00945e
Matthew Regan Hadiwidjaja <mhadiwidjaja@student.ciputra.ac.id>
2025-05-22 09:29:25 +0700

    Initial Commit

---
+++ SpareTrousers/SpareTrousersApp.swift
@@ -0,0 +1,17 @@
+//
+//  SpareTrousersApp.swift
+//  SpareTrousers
+//
+//  Created by student on 22/05/25.
+//
+
+import SwiftUI
+
+@main
+struct SpareTrousersApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
