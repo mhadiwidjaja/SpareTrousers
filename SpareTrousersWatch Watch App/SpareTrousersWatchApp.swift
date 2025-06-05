df60212a7a1a6bff4fab927be01e763fcb69fdee
Matthew Regan Hadiwidjaja <mhadiwidjaja@student.ciputra.ac.id>
2025-06-05 12:51:58 +0700

    Setup for MacOS and WatchOS

---
+++ SpareTrousersWatch Watch App/SpareTrousersWatchApp.swift
@@ -0,0 +1,32 @@
+//
+//  SpareTrousersWatchApp.swift
+//  SpareTrousersWatch Watch App
+//
+//  Created by student on 05/06/25.
+//
+
+import SwiftUI
+import FirebaseCore
+
+
+class AppDelegate: NSObject, UIApplicationDelegate {
+  func application(_ application: UIApplication,
+                   didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey : Any]? = nil) -> Bool {
+    FirebaseApp.configure()
+
+    return true
+  }
+}
+
+@main
+struct SpareTrousersWatchApp: App {
+  @UIApplicationDelegateAdaptor(AppDelegate.self) var delegate
+
+  var body: some Scene {
+    WindowGroup {
+      NavigationView {
+        ContentView()
+      }
+    }
+  }
+}
